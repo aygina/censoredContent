02977d0256bad3f3223b05e41a99dd1fb558c1f1
aygina <aygina.v@gmail.com>
2022-11-23 23:45:19 +0300

    DONE

--- index.js
+++ index.js
@@ -1,24 +1,9 @@
-function xx () {
+function checkSpam() {
     let commentSect = document.querySelector('#commentSection');
-    let comment = document.getElementById('comment').value + '\n';
-    document.getElementById('comment').value = '';
-
-    commentSect.append(comment);
-}
-
-function submit() {
     let comment = document.getElementById('comment').value;
     let spam = /viagra|xxx/gi;
+
     newComm = comment.replace(spam, "***");
-    console.log(newComm);
+    commentSect.append(newComm + '\n');
+    document.getElementById('comment').value = '';
 }
-
-
-// функция смотретьСпам () {
-//   Записываешь сообщение, которое ввёл пользователь
-//   Меняешь регистр
-//   Создаёшь переменную, которая содержит спам (нужны флаги)
-//   Заменяешь спам, если он есть
-//   Добавляешь строку в чат
-//   Очищаешь поле ввода
-// }
